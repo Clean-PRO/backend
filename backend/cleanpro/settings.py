c1872b79e8f26a8ca49933091a71044387d20647
Владимир <119314112+Star-memory@users.noreply.github.com>
2023-11-05 14:13:10 +0300

    Update settings.py

--- backend/cleanpro/settings.py
+++ backend/cleanpro/settings.py
@@ -54,23 +54,23 @@ CACHES = {
     }
 }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': DB_ENGINE,
-#         'NAME': DB_NAME,
-#         'USER': DB_USER,
-#         'PASSWORD': DB_PASSWORD,
-#         'HOST': DB_HOST,
-#         'PORT': DB_PORT,
-#     }
-# }
-# TODO: Пока оставить тут, используется для тестов. При релизе - удалить.
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': DB_ENGINE,
+        'NAME': DB_NAME,
+        'USER': DB_USER,
+        'PASSWORD': DB_PASSWORD,
+        'HOST': DB_HOST,
+        'PORT': DB_PORT,
     }
 }
+# TODO: Пока оставить тут, используется для тестов. При релизе - удалить.
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
