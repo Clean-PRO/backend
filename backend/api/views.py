27f8bc7576fc5266465f48e1b08d94639e29f62b
Kirill Svidunovich <Altair21817@gmail.com>
2023-11-08 18:17:33 +0300

    Add confirm_email and confirm_password, refactor permissions and serializers

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,8 @@
 # TODO: аннотировать типы данных. Везде. Абсолютно.
+# TODO: Сделать хорошие docstring. Везде.
 
+from django.db.models import QuerySet
+from django.contrib.auth import authenticate
 from django.http import HttpRequest
 from django.shortcuts import get_object_or_404
 from django_filters import rest_framework as filters
@@ -14,18 +17,17 @@ from drf_spectacular.utils import (
 )
 
 from api.filters import FilterService
-from api.mixin import CreateUpdateListSet
 from api.permissions import (
     IsAdminOrReadOnly,
-    IsNotAdmin,
-    IsOwner,
+    IsOwnerOrAdmin,
+    IsOwnerAbleToPay,
 )
 from api.serializers import (
     AdminOrderPatchSerializer,
     CleaningGetTimeSerializer,
     CreateCleaningTypeSerializer,
     CreateServiceSerializer,
-    EmailConfirmSerializer,
+    EmailVerifySerializer,
     GetCleaningTypeSerializer,
     GetServiceSerializer,
     MeasureSerializer,
@@ -33,14 +35,15 @@ from api.serializers import (
     OrderPostSerializer,
     OrderRatingSerializer,
     OwnerOrderPatchSerializer,
+    PasswordConfirmSerializer,
     PaySerializer,
     RatingSerializer,
-    UserCreateSerializer,
-    UserGetSerializer,
+    UserSerializer,
+    UserRegisterSerializer,
 )
-from api.utils import generate_code, get_available_time_json, send_mail
+from api.utils import create_password, get_available_time_json, send_mail
 from cleanpro.app_data import (
-    EMAIL_CONFIRM_CODE_TEXT, EMAIL_CONFIRM_CODE_SUBJECT,
+    EMAIL_CONFIRM_EMAIL_SUBJECT, EMAIL_CONFIRM_EMAIL_TEXT, SERVICES_ADDITIONAL,
 )
 from .schemas_config import (
     TYPES_CLEANING_SCHEMA,
@@ -50,7 +53,6 @@ from .schemas_config import (
     ORDER_SCHEMA,
     USER_SCHEMA,
 )
-from cleanpro.settings import ADDITIONAL_CS
 from services.models import CleaningType, Measure, Order, Rating, Service
 from services.signals import get_cached_reviews
 from users.models import User
@@ -60,9 +62,10 @@ from users.models import User
 @extend_schema_view(**MEASURE_SCHEMA)
 class MeasureViewSet(viewsets.ModelViewSet):
     """Работа с единицами измерения услуг."""
+
     queryset = Measure.objects.all()
     serializer_class = MeasureSerializer
-    permission_classes = (permissions.IsAuthenticated, IsAdminOrReadOnly,)
+    permission_classes = (IsAdminOrReadOnly,)
     pagination_class = None
     http_method_names = ('get', 'post', 'put',)
 
@@ -70,7 +73,8 @@ class MeasureViewSet(viewsets.ModelViewSet):
 @extend_schema(tags=["Types cleaning"])
 @extend_schema_view(**TYPES_CLEANING_SCHEMA)
 class CleaningTypeViewSet(viewsets.ModelViewSet):
-    """Работа с типами услуг."""
+    """Работа с наборами услуг."""
+
     queryset = CleaningType.objects.prefetch_related('service').all()
     permission_classes = (IsAdminOrReadOnly,)
     pagination_class = None
@@ -87,6 +91,7 @@ class CleaningTypeViewSet(viewsets.ModelViewSet):
 @extend_schema_view(**SERVICE_SCHEMA)
 class ServiceViewSet(viewsets.ModelViewSet):
     """Работа с услугами."""
+
     queryset = Service.objects.select_related('measure').all()
     permission_classes = (IsAdminOrReadOnly,)
     filter_backends = (filters.DjangoFilterBackend,)
@@ -95,8 +100,7 @@ class ServiceViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         if not self.request.user.is_staff:
-            self.pagination_class = None
-            return self.queryset.filter(service_type=ADDITIONAL_CS)
+            return self.queryset.filter(service_type=SERVICES_ADDITIONAL)
         else:
             return self.queryset
 
@@ -109,17 +113,23 @@ class ServiceViewSet(viewsets.ModelViewSet):
 
 @extend_schema(tags=["User"])
 @extend_schema_view(**USER_SCHEMA)
-# TODO: Наставник написал, какой смысл создавать CreateUpdateListSet.
-#       Я так подумал, этот список миксинов используется один раз. 
-#       Смысла в нем, не очень много. Может быть имеет смысл прямо
-#       тут и перечислить их, а не наследоваться.
-class UserViewSet(CreateUpdateListSet):
+class UserViewSet(viewsets.ModelViewSet):
+    """Работа с пользователями."""
+
     queryset = User.objects.select_related('address').all()
+    http_method_names = ('get', 'post', 'put',)
 
     def get_serializer_class(self):
         if self.request.method == 'POST':
-            return UserCreateSerializer
-        return UserGetSerializer
+            return UserRegisterSerializer
+        return UserSerializer
+
+    def get_permissions(self):
+        if self.request.method == 'POST':
+            return [permissions.AllowAny()]
+        if self.request.method == 'GET':
+            return [permissions.IsAdminUser()]
+        return [IsOwnerOrAdmin()]
 
     @action(
         detail=True,
@@ -129,8 +139,12 @@ class UserViewSet(CreateUpdateListSet):
     )
     def orders(self, request, pk):
         queryset = Order.objects.filter(
-            user=pk
-        ).select_related('user', 'cleaning_type', 'address')
+            user=pk,
+        ).select_related(
+            'user',
+            'cleaning_type',
+            'address',
+        )
         page = self.paginate_queryset(queryset)
         serializer = OrderGetSerializer(
             page,
@@ -141,12 +155,13 @@ class UserViewSet(CreateUpdateListSet):
 
     @action(
         detail=False,
+        url_path='me',
         methods=('get',),
         permission_classes=(permissions.IsAuthenticated,)
     )
     def me(self, request):
         instance: User = request.user
-        serializer: UserGetSerializer = UserGetSerializer(instance)
+        serializer: UserSerializer = UserSerializer(instance)
         data: ReturnDict = serializer.data
         for attribute in ('is_staff', 'is_cleaner'):
             if getattr(self.request.user, attribute):
@@ -160,18 +175,72 @@ class UserViewSet(CreateUpdateListSet):
         permission_classes=(permissions.AllowAny,)
     )
     def confirm_email(self, request):
-        serializer: serializers = EmailConfirmSerializer(data=request.data)
+        """
+        Производит валидацию введенного email и производит проверку
+        существования пользователя с указанной почтой.
+        Если пользователя не существует: генерирует уникальный пароль
+        учетной записи для авторизации на сайте при осуществлении заказа.
+        """
+        serializer: serializers = EmailVerifySerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        confirm_code: str = generate_code()
-        send_mail(
-            subject=EMAIL_CONFIRM_CODE_SUBJECT,
-            message=EMAIL_CONFIRM_CODE_TEXT.format(confirm_code=confirm_code),
-            to=(serializer.validated_data.get('email'),),
-        )
+        email: str = serializer.validated_data.get('email')
+        if User.objects.filter(email=email).exists():
+            data: dict[str, str] = {
+                'exists': 'Введите пароль вашей учетной записи:',
+            }
+        else:
+            new_password: str = create_password(email=email)
+            send_mail(
+                subject=EMAIL_CONFIRM_EMAIL_SUBJECT,
+                message=EMAIL_CONFIRM_EMAIL_TEXT.format(password=new_password),
+                to=(email,)
+            )
+            data: dict[str, str] = {
+                'created': (
+                    'Введите пароль вашей учетной записи, '
+                    'который был выслан на указанную почту:'
+                ),
+            }
         return Response(
-            data={"confirm_code": confirm_code},
+            data=data,
             status=status.HTTP_200_OK,
         )
+
+    @action(
+        detail=False,
+        url_path='confirm_password',
+        methods=('post',),
+        permission_classes=(permissions.AllowAny,)
+    )
+    def confirm_password(self, request):
+        """
+        Если пользователь с указанно почтой существует: проверяет возможность
+        авторизации с предоставленными данными.
+        Если пользователя не существует - проверяет валидность указанного
+        пароля и далее возможность авторизации.
+        """
+        serializer: serializers = PasswordConfirmSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        email: str = serializer.validated_data.get('email')
+        password: str = serializer.validated_data.get('password')
+        user: QuerySet = User.objects.filter(email=email)
+        if not user.exists() and password == create_password(email=email):
+            user: User = User.objects.create(email=email)
+            user.set_password(password)
+            user.save()
+        else:
+            user: User = user.first()
+        authentication: User = authenticate(
+            email=email,
+            password=password
+        )
+        if authentication is None:
+            data: dict = {'password': 'Указан неверный пароль.'}
+            status_code: status = status.HTTP_400_BAD_REQUEST
+        else:
+            data: dict = {'password': 'Пароль успешно подтвержден.'}
+            status_code: status = status.HTTP_200_OK
+        return Response(data=data, status=status_code)
 
 
 @extend_schema(tags=["Order"])
@@ -180,13 +249,12 @@ class OrderViewSet(viewsets.ModelViewSet):
     """Список заказов."""
     http_method_names = ('get', 'post', 'patch', 'put',)
     queryset = Order.objects.select_related('user', 'address',).all()
-    # TODO: так как переопределяется класс полностью, вынести это отсюда
-    #       и прописать в get_permissions(self)
-    permission_classes = (IsOwner,)
 
     def get_permissions(self):
         if self.request.method == 'POST':
-            self.permission_classes = (permissions.AllowAny,)
+            self.permission_classes = (permissions.IsAuthenticated,)
+        else:
+            self.permission_classes = (IsOwnerOrAdmin,)
         return super().get_permissions()
 
     def get_queryset(self):
@@ -205,26 +273,11 @@ class OrderViewSet(viewsets.ModelViewSet):
                 return OwnerOrderPatchSerializer
         return OrderPostSerializer
 
-    def __modify_order(
-            self,
-            order_id,
-            request: HttpRequest,
-            serializer_class: serializers,
-            ) -> serializers:  # NoQa
-        order = get_object_or_404(Order, id=order_id)
-        request.data['id'] = order.id
-        serializer = serializer_class(
-            order,
-            request.data,
-            context={'request': request})
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return serializer
-
     @action(
         detail=True,
         methods=('post', 'put',),
         url_path='rating',
+        permission_classes=(IsOwnerOrAdmin,)
     )
     def rating(self, request, pk):
         """Оценить заказ."""
@@ -248,10 +301,8 @@ class OrderViewSet(viewsets.ModelViewSet):
 
     @action(
         detail=True,
-        methods=('patch',),
-        # TODO: сделать разрешение только создателю заказа.
-        #       Можно без администратора, мысль здравая.
-        permission_classes=(IsNotAdmin,),
+        methods=('post',),
+        permission_classes=(IsOwnerAbleToPay,),
         url_path='pay',
     )
     def pay(self, request, pk):
@@ -282,6 +333,22 @@ class OrderViewSet(viewsets.ModelViewSet):
             ),
             status=status.HTTP_200_OK,
         )
+
+    def __modify_order(
+            self,
+            order_id,
+            request: HttpRequest,
+            serializer_class: serializers,
+            ) -> serializers:  # noqa (E123)
+        order = get_object_or_404(Order, id=order_id)
+        request.data['id'] = order.id
+        serializer = serializer_class(
+            order,
+            request.data,
+            context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return serializer
 
 
 @extend_schema(tags=["Rating"])
