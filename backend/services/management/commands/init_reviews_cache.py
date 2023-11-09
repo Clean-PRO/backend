96512c5db6629584a7981d494e161aa83da2d5af
Kirill Svidunovich <Altair21817@gmail.com>
2023-11-09 13:49:59 +0300

    Add parse_yandex_maps

--- backend/services/management/commands/init_reviews_cache.py
+++ backend/services/management/commands/init_reviews_cache.py
@@ -7,6 +7,7 @@ python manage.ru init_reviews_cache
 
 from django.core.management.base import BaseCommand, CommandError
 
+from services.tasks import parse_yandex_maps
 from services.signals import update_cached_reviews
 
 
@@ -15,6 +16,7 @@ class Command(BaseCommand):
 
     def handle(self, *args: any, **options: any):
         try:
+            parse_yandex_maps()
             update_cached_reviews()
         except Exception as err:
             raise CommandError(f'Exception has occurred: {err}')
