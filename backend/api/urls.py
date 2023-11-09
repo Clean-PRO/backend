b522de546dfc8de15bec1ebc78e33f6bb49be9fb
Kirill Svidunovich <Altair21817@gmail.com>
2023-11-09 03:07:09 +0300

    Change cleaning-types URL

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -16,11 +16,11 @@ app_name = 'api'
 
 router = routers.DefaultRouter()
 ROUTER_DATA = (
+    ('cleaning-types', CleaningTypeViewSet,),
     ('measure', MeasureViewSet,),
     ('orders', OrderViewSet,),
     ('ratings', RatingViewSet,),
     ('services', ServiceViewSet),
-    ('types', CleaningTypeViewSet,),
     ('users', UserViewSet,),
 )
 for api_path in ROUTER_DATA:
